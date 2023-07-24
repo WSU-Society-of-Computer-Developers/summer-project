9d391568d6e564b42bd97ad4c2368216f019169d
andrew-adomaitis <theelderone02@gmail.com>
2023-07-24 17:01:05 -0400

    Added Basic styling

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {MDCTextField} from '@material/textfield'
+import { Container, TextField } from '@mui/material'
+import "./SignUp.css"
 
 function SignUp() {
     return (
         <>
-            <h1>Testing</h1>
+        <header>
+            <h1 className="text-4xl font-bold">Sign Up to ByteBound WSU</h1>
+            <h2>To register, please enter the following information</h2>
+        </header>
+        <Container className='info-container'>
+            <TextField label="Email Address" variant="standard" color='secondary' />
+            <TextField label="Username" variant="standard" />
+            <TextField label="Password" variant="standard" />
+        </Container>
         </>
     );
 }
