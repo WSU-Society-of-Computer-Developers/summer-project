a199c0e93ed15b877ef41f6ce9dddcab22d31ec4
andrew-adomaitis <theelderone02@gmail.com>
2023-08-02 15:38:41 -0400

    Started footer rework

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,9 @@
 function Footer() {
-  return (
+  /*return (
     // TODO: fix footer going over other elements
     <div>
       <div className="sticky m-24"></div>
-      <footer className="fixed bottom-0 m-4 rounded-lg bg-white text-black shadow dark:bg-gray-200">
+      <footer className="bottom-0 m-4 rounded-lg bg-white text-black shadow dark:bg-gray-200">
         <div className="mx-auto w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-sm sm:text-center">
             © {new Date().getFullYear().toString() + ' '}
@@ -41,6 +41,46 @@ function Footer() {
         </div>
       </footer>
     </div>
+  )*/
+  return (
+    <>
+      <footer className="secondary">
+        <h3 className="text-center text-xl">ByteBound WSU</h3>
+        <ul className="flex flex-col mt-3 items-center text-sm font-medium sm:mt-0">
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">
+                Licensing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline md:mr-6">
+                Contact
+              </a>
+            </li>
+          </ul>
+          <span className="text-sm sm:text-center">
+            © {new Date().getFullYear().toString() + ' '}
+            <a
+              href="https://scd.cs.wayne.edu"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:underline"
+            >
+              SCD
+            </a>
+          </span>
+      </footer>
+    </>
   )
 }
 
