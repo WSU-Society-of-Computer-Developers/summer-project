b63fd86f256518e9fb1ca18d60411c60683b9055
ThatZiv <thatziv@gmail.com>
2023-08-03 01:20:26 -0400

    weird cache bug

--- client/src/pages/posts/Post.tsx
+++ client/src/pages/posts/Post.tsx
@@ -31,7 +31,8 @@ function Post() {
   // this is how you would do it w/ strictly backend
   // NOTE: you would have to change data to data.body (bcz we're using the backend)
   // const { data, error } = useSWR('/posts/' + postid, fetcher)
-  const { data, error, mutate } = useSWR({}, () =>
+  const random = React.useRef(Date.now()) // used to force refresh the data (im starting to regret using swr)
+  const { data, error, mutate } = useSWR([random], () =>
     api.posts.get(postid, {
       expand: 'author,comments(post).author,likes(post)'
     })
