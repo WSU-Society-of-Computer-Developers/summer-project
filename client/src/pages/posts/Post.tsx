e0bc32821d7a0e6c0452d0db596a089137be872d
ThatZiv <thatziv@gmail.com>
2023-08-23 12:28:40 -0400

    changed reply name to comment

--- client/src/pages/posts/Post.tsx
+++ client/src/pages/posts/Post.tsx
@@ -380,7 +380,7 @@ function Post() {
                     }
                   }}
                 >
-                  Reply
+                  Comment
                 </Button>
               </>
             )}
